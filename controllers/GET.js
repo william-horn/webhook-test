f380564e95fce657854de06d2596b2df065b5ae0
William Horn <williamjosephhorn@gmail.com>
2022-05-15 12:57:39 -0400

    update 3

--- controllers/GET.js
+++ controllers/GET.js
@@ -1,7 +1,12 @@
 
 const router = require('express').Router();
+const path = require('path');
 
 router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+router.get('api/', (req, res) => {
     res.json({ message: 'success' });
 });
 
--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const routes = require('./controllers');
 const app = express();
 
 app.use(routes);
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
 app.listen(3001, () => console.log('Running server'));
 
