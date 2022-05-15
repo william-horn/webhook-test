c8515821c3c0d2246e48e1b2e8ef526db62a6423
William Horn <williamjosephhorn@gmail.com>
2022-05-15 12:37:50 -0400

    update 1

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ const routes = require('./controllers');
 const app = express();
 
 app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.listen(3001, () => console.log('Running server'));
 
